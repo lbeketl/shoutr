9e528a52d4eb1fc782d531c898c1989b99e077eb
beket <lbeketl@mail.com>
2021-05-12 22:19:02 +0300

    Allow user to shout

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,16 @@
 class HomesController < ApplicationController
+  # before_action :redirect_to_dashboard_if_signed_in
   
   def show
     render 
   end
+
+  # private
+
+  # def redirect_to_dashboard_if_signed_in
+  #   if signed_in?
+  #     redirect_to dashboard_path
+  #   end
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'homes#show'
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'dashboards#show'
+  end
+  
+  root to: 'homes#show', as: :homes_url
+  resources :shouts, only: [:create]
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
